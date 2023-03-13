a72050dbea62addfa715191057400101cdf94ccb
ixec-lab <islem.meghnine06@gmail.com>
2023-03-13 14:17:15 +0100

    UPDATE: firewall rules

--- modules/subnet/gcp/firewall.tf
+++ modules/subnet/gcp/firewall.tf
@@ -1,4 +1,24 @@
-resource "google_compute_firewall" "private_subnet_outside_rule" {
+resource "google_compute_firewall" "private_subnet_authorized_inside_rule" {
+    name     = "firewall-private-subnet-inside-rule"
+    network  = var.vpc_name
+
+    allow {
+      protocol = "tcp"
+      ports    = [ "0-65535" ]
+    }
+
+    allow {
+      protocol = "udp"
+      ports    = [ "0-65535"]
+    }
+
+    priority = 1002
+
+    destination_ranges = [for subnet in local.private_subnets: subnet.cidr_block]
+    source_ranges = [for subnet in local.private_subnets: subnet.cidr_block]
+}
+
+resource "google_compute_firewall" "private_subnet_forbidden_outside_rule" {
     name     = "firewall-private-subnet-ouside-rule"
     network  = var.vpc_name
 
